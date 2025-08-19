d9fd729100d26805b825d02d9de173352ceaf305
alonablak <136439532+alonablak@users.noreply.github.com>
2025-08-19 10:02:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ projects = ["Project A", "Project B", "Project C", "Project D"]
 print("Dự án ngẫu nhiên:", random.choice(projects))
 --v.0.0.03
 
---02--03--04--05--06--07--11--15--17--18
+--02--03--04--05--06--07--11--15--17--18--19
